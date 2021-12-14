f8935232535b06b165522286290fcb767f12209c
vins_stha <shrs_samar@yahoo.com>
2021-12-14 23:33:40 +0200

    updates to routes and homepage

--- src/components-factory/Layout/Header.js
+++ src/components-factory/Layout/Header.js
@@ -3,15 +3,15 @@ import '../../assets/styles/styles.css';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
 import Cookies from 'universal-cookie'
-
- const cookie = new Cookies();
+import {Link} from 'react-router-dom'
+const cookie = new Cookies();
 
 export const Header = () => {
     const [username, setUsername] = useState([]);
     const [password, setPassword] = useState([]);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
-  
+
     const data = {
         email: username,
         password
@@ -21,74 +21,79 @@ export const Header = () => {
         console.log('data=', data)
         let result = axios.post("http://127.0.0.1:8000/api/login", data, {
             headers: {
-            "Content-type": "application/json",
-           }},)
-        .then((res)=>{
-            console.log('res=>',res);
-            cookie.set('isLoggedInToken', true,{ path: '/' });
-            console.log('isLoggedInToken=>',cookie.get('isLoggedInToken'));
-            navigate('/dashboard');
-           
-            setIsLoggedIn(true);
-
+                "Content-type": "application/json",
+            }
         })
-        .catch((error)=>{
-            console.log('error', error)
-        })
+            .then((res) => {
+                console.log('res=>', res);
+                cookie.set('isLoggedInToken', true, { path: '/' });
+                console.log('isLoggedInToken=>', cookie.get('isLoggedInToken'));
+                navigate('/dashboard');
 
-      
+                setIsLoggedIn(true);
+
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+
+
     }
     const handleLogout = (e) => {
         e.preventDefault();
-      
-        cookie.set('isLoggedInToken', false, {path : '/'});
-       
+
+        cookie.set('isLoggedInToken', false, { path: '/' });
+
         navigate('/')
     }
 
     useEffect(() => {
-       if( cookie.get('isLoggedInToken') === "undefined")  {cookie.set('isLoggedInToken', false, {path : '/'})};
+        if (cookie.get('isLoggedInToken') === "undefined") { cookie.set('isLoggedInToken', false, { path: '/' }) };
         console.log('isLoggedIn', isLoggedIn)
     }, [])
 
     return (
         <div>
-            {window.location.pathname.indexOf('admin') === 1 ? "":(
-    <div className="header flex">
-    <div className="header-logo flex">Nav bar Logo</div>
-    <div className="header-registration flex-column">
-  
-        { cookie.get('isLoggedInToken') === "true" ?
-            <div>
-                <div className="header-registration__links flex">
-                    <a className="header-registration__links__forgot" href="">Change  Password</a>
-                    <a href="/register" className="header-registration__links__register">Profile</a>
-                    <div className="btn btn-primary"onClick={handleLogout} className="header-registration__links__register">Logout</div>
+            {window.location.pathname.indexOf('admin') === 1 ? "" : (
+                <div className="header flex">
+                    <div className="header-logo flex">Dream-Estate</div>
+                    <div className="header-home-menu flex">
+                    <a href="/real-estate">Home</a>
+                    
+                        <a href="/real-estate/search">Search-Homes</a>
+                       
+                    </div>
+                    
+                    <div className="header-registration flex-column">
+
+                        {cookie.get('isLoggedInToken') === "true" ?
+                            <div>
+                                <div className="header-registration__links flex">
+                                    <a className="header-registration__links__forgot" href="">Change  Password</a>
+                                    <a href="/register" className="header-registration__links__register">Profile</a>
+                                    <div className="btn btn-primary" onClick={handleLogout} className="header-registration__links__register">Logout</div>
+                                </div>
+                            </div>
+
+                            :
+
+                            <div>
+                                <div className="header-registration__links flex">
+                                    <a className="header-registration__links__forgot" href="">Forgot Username</a>
+                                    <a href="/register" className="header-registration__links__register">Register</a>
+                                </div>
+                                <div className="header-registration__login">
+                                    <input placeholder="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
+                                    <input placeholder="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                                    <div className="btn btn-primary" onClick={handleLogin}>login</div>
+                                </div>
+                            </div>
+                        }
+
+                    </div>
+
                 </div>
-            </div>
-            
-            :
-
-            <div>
-                <div className="header-registration__links flex">
-                    <a className="header-registration__links__forgot" href="">Forgot Username</a>
-                    <a href="/register" className="header-registration__links__register">Register</a>
-                </div>
-                <div className="header-registration__login">
-                    <input placeholder="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
-                    <input placeholder="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-                    <div className="btn btn-primary" onClick={handleLogin}>login</div>
-                </div>
-            </div>
-        }
-
-    </div>
-
-</div>
-            ) }
-        
-
-
+            )}
         </div>
     )
 }
